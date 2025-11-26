eb8010154d016cf12e27021096366406a064a5f4
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-11-26 15:36:51 +0500

    new changes

--- counter-app/src/Counter.jsx
+++ counter-app/src/Counter.jsx
@@ -1,40 +1,37 @@
 import { useState } from "react";
-import './Counter.css'
+import "./Counter.css";
 
 function Counter() {
+  const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0);
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
 
-    const increaseCount = () => {
-        setCount(count + 1);
+  const decreaseCount = () => {
+    if (count === 0) {
+      alert("Can not Count in negeative");
+    } else {
+      setCount(count - 1);
     }
+  };
 
-    const decreaseCount = () => {
-        if (count === 0) {
-            alert("Can not Count in negeative")
-        } else {
-            setCount(count - 1);
-        }
-    }
+  const resetCount = () => {
+    setCount(0);
+  };
 
-    const resetCount = () => {
-        setCount(0);
-    }
+  return (
+    <div className="counter">
+      <button onClick={increaseCount}>Increase</button>
 
-    return (
+      <p className="count">{count}</p>
 
-        <div className="counter">
-
-            <button onClick={increaseCount}>Increase</button>
-
-            <p className="count">{count}</p>
-
-            <button onClick={decreaseCount}>Decrease</button>
-            <button className="reset" onClick={resetCount} disabled={count === 0}>Reset</button>
-
-        </div>
-
-    );
+      <button onClick={decreaseCount}>Decrease</button>
+      <button className="reset" onClick={resetCount} disabled={count === 0}>
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default Counter;
