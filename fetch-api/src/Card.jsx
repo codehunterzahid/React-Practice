b9679c8adb9a8d2b5f3ea40260dba159cd1a2c1a
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-11-26 15:37:31 +0500

    new

--- fetch-api/src/Card.jsx
+++ fetch-api/src/Card.jsx
@@ -4,13 +4,13 @@ function Card({ price, title, bio }) {
 
   return (
     <div className="card">
-    <img src="{img}" alt="" />
+      <img src="{img}" alt="" />
       <h2 className="name">{price}</h2>
       <p className='title'>{title}</p>
       <p className='bio'>{bio}</p>
     </div>
   );
-  
+
 }
 
 export default Card;
