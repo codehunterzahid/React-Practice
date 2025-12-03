d476ccc0b59e2d43885c03b0d0b60302f7cec8e6
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-03 13:35:20 +0500

    new app created

--- route-app/src/App.jsx
+++ route-app/src/App.jsx
@@ -1,11 +1,24 @@
-
+import Navbar from './Navbar.jsx'
+import Home from './Home.jsx'
+import About from './About.jsx'
+import Services from './Services.jsx'
+import Contact from './Contact.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ route-app/src/Navbar.jsx
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+
+            <nav>
+                <NavLink to="/home">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/services">Services</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </nav>
+
+    )
+}
+
+export default Navbar;
