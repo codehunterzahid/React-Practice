8b2d8f38907d7dd692b46adeae4560a7d6c7c188
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-03 13:36:25 +0500

    Navbar

--- route-app/src/Navbar.jsx
+++ route-app/src/Navbar.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
 
-            <nav>
-                <NavLink to="/home">Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/services">Services</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-            </nav>
+        <nav>
+            <NavLink to="/home">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/services">Services</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+        </nav>
 
     )
 }
