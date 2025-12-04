6a9a4530d5b272d4cca1a25d01d67df960f204f3
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-04 17:46:49 +0500

    code completed

--- redux-app/src/App.jsx
+++ redux-app/src/App.jsx
@@ -1,14 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { incNumber, decNumber } from './actions/index';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const myState = useSelector((state) => state.changeCounter);
+  const dispatch = useDispatch();
   return (
     <>
-    <h1>This is Redux Counter</h1>
+
+      <div className='container'>
+
+        <h1>This is a Counter App</h1>
+        <h3>Using React Redux</h3>
+
+        <div className='counter'>
+          <button onClick={() => dispatch(decNumber())}>-</button>
+
+          <input
+            type="text"
+            value={myState}
+            readOnly
+          />
+
+          <button onClick={() => dispatch(incNumber())}>+</button>
+        </div>
+
+      </div>
+
     </>
   )
 }
---
+++ redux-app/src/reducers/index.jsx
@@ -0,0 +1,8 @@
+import changeCounter from "./Counter";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    changeCounter
+})
+
+export default rootReducer;
