830ec7277141ac6159f2616b0472d6b0c47d744f
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-04 17:47:56 +0500

    counter changes

--- redux-app/src/reducers/Counter.jsx
+++ redux-app/src/reducers/Counter.jsx
@@ -1,9 +1,9 @@
 const initialState = 0;
 
-const changeCounter = (state = initialState, action) =>{
-    switch(action.type){
+const changeCounter = (state = initialState, action) => {
+    switch (action.type) {
         case "INCREMENT": return state + 1;
-        case "DECREMENT" : return state - 1;
+        case "DECREMENT": return state - 1;
         default: return state;
     }
 }
