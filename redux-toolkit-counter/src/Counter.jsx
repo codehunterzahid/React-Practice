a56d56a7fa3cb801834aeb0b079a4d7b17c65c84
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-08 13:36:09 +0500

    new changes

--- redux-toolkit-counter/src/Counter.jsx
+++ redux-toolkit-counter/src/Counter.jsx
@@ -1,25 +1,23 @@
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement, incrementByAmount } from "./counterSlice";
+import "./Counter.css";
 
 function Counter() {
-    const count = useSelector((state) => state.counter.value);
-    const dispatch = useDispatch();
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
 
-    return (
+  return (
+    <div className="counter-container">
+      <h1>Redux Toolkit Counter</h1>
+      <h2>{count}</h2>
 
-        <div style={{ textAlign: "center", marginTop: "50px" }}>
-            <h1>Redux Toolkit Counter</h1>
-            <h2>Count: {count}</h2>
-
-
-            <button onClick={() => dispatch(decrement())}>-</button>
-            <button onClick={() => dispatch(increment())}>+</button>
-
-            <button onClick={() => dispatch(incrementByAmount(5))}>
-                +5
-            </button>
-        </div>
-    )
+      <div className="counter-btns">
+        <button className="btn-minus" onClick={() => dispatch(decrement())}>-</button>
+        <button className="btn-plus" onClick={() => dispatch(increment())}>+</button>
+        <button className="btn-add" onClick={() => dispatch(incrementByAmount(5))}>+5</button>
+      </div>
+    </div>
+  );
 }
 
 export default Counter;
