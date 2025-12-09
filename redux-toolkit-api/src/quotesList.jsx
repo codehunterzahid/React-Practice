6374a2074e12a4b9cdf1c182d09cd87254917dee
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-09 12:49:02 +0500

    new changes

---
+++ redux-toolkit-api/src/quotesList.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchQuotes } from "./quoteSlice";
+import './quoteList.css'
+
+
+function QuotesList() {
+    const dispatch = useDispatch();
+
+    const { quotes, loading, error } = useSelector((state) => state.quotes)
+
+    useEffect(() => {
+        dispatch(fetchQuotes());
+    }, [dispatch]);
+
+    if (loading) return <h2>Loading...</h2>
+    if (error) return <h2>Error: {error}</h2>
+
+    return (
+    
+        <div className="quotes-container">
+            {quotes.map((q) => (
+                <div className="quote-card" key={q.id}>
+                    <p className="quote-text">{q.quote}</p>
+                    <h3 className="quote-author">{q.author}</h3>
+                </div>
+            ))}
+
+
+        </div>
+    )
+}
+
+export default QuotesList;
