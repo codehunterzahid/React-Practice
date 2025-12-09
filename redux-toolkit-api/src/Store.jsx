76f521bd01cfc0256e29e91063f945cd54e9687e
codehunterzahid <codehuntingwithzahid@gmail.com>
2025-12-09 12:50:24 +0500

    STore configured

--- redux-toolkit-api/src/Store.jsx
+++ redux-toolkit-api/src/Store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import quoteReducer from "./quoteSlice";
 
 export const store = configureStore({
-  reducer: {
-    quotes: quoteReducer,
-  },
+    reducer: {
+        quotes: quoteReducer,
+    },
 });
